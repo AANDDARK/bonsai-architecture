1dfb8a99ba573683455907a1de69a1f9ba25ec33
Andrii Kovalenko <andriyworkkovalenko@gmail.com>
2025-08-02 22:31:30 +0300

    some tree changes

---
+++ src/thunk/leaf/button.vue
@@ -0,0 +1,10 @@
+<template>
+    <button @click="props.func()"></button>
+</template>
+<script setup lang="js">
+    const props = defineProps({
+        func: {
+            Type: Function,
+        }
+    })
+</script>
